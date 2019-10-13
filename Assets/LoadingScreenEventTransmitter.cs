25e5b92b388218c708da73e71e3e94c0fc735e67
Иван Хомичков <ivan.khomichkov@gmail.com>
2019-10-13 19:35:33 +0300

    Initial commit

---
+++ Assets/LoadingScreenEventTransmitter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class LoadingScreenEventTransmitter : MonoBehaviour
+{
+
+    public event Action OnAnimationFinished;
+    public event Action OnAnimationStart;
+
+    private Animator animator;
+
+    public int NumberOfClips { get; private set; }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        NumberOfClips = animator.GetNextAnimatorClipInfoCount(0);
+    }
+
+    public void AnimationEventOnFinishedCalled()
+    {
+        if (Debug.isDebugBuild) Debug.Log($"<color=purple>GETCALLED</color>");
+        
+        OnAnimationFinished?.Invoke();
+    }
+
+    public void AnimationEventOnStartCalled()
+    {
+        OnAnimationStart?.Invoke();
+    }
+}
---
+++ Assets/LoadingSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingSystem : MonoBehaviour
+{
+    [SerializeField] private LoadingScreenEventTransmitter loadingScreenEventTransmitter;
+
+    private bool thereAreAnimationsInsideLoadingScreen;
+    private void Awake()
+    {
+        if (loadingScreenEventTransmitter == null)
+        {
+            loadingScreenEventTransmitter = GetComponentInChildren<LoadingScreenEventTransmitter>();
+            
+            thereAreAnimationsInsideLoadingScreen = loadingScreenEventTransmitter != null;
+            
+        }
+        else
+        {
+            thereAreAnimationsInsideLoadingScreen = true;
+        }
+
+        if (thereAreAnimationsInsideLoadingScreen)
+        {
+            //mytodo probably delete this later
+            if (Debug.isDebugBuild) Debug.Log($"<color=purple>{loadingScreenEventTransmitter.NumberOfClips}</color>");
+            loadingScreenEventTransmitter.OnAnimationFinished += InsideAnimationFinishedHandler;
+            loadingScreenEventTransmitter.OnAnimationStart += InsideAnimationStartedHandler;
+        }
+    }
+
+    private void InsideAnimationStartedHandler()
+    {
+        //mytodo probably delete this later
+        if (Debug.isDebugBuild) Debug.Log($"<color=purple>LoadingSystem know that Animations Inside started</color>");
+    }
+
+    private void InsideAnimationFinishedHandler()
+    {
+        //mytodo probably delete this later
+        if (Debug.isDebugBuild) Debug.Log($"<color=purple>LoadingSystem know that Animations Inside finished</color>");
+    }
+}
